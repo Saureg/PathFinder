de2a4c146a554ecaea89e87697d0c7a4ff26ba45
Egor Uvarov <e.uvarov@pravo.ru>
2020-05-14 20:27:43 +0400

    Transactions

---
+++ PathFinder/Migrations/20200512183620_AddTraitsInRace.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PathFinder.Migrations
+{
+    public partial class AddTraitsInRace : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChaTrait",
+                table: "Races",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConTrait",
+                table: "Races",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DexTrait",
+                table: "Races",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IntTrait",
+                table: "Races",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StrTrait",
+                table: "Races",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WisTrait",
+                table: "Races",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChaTrait",
+                table: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "ConTrait",
+                table: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "DexTrait",
+                table: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "IntTrait",
+                table: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "StrTrait",
+                table: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "WisTrait",
+                table: "Races");
+        }
+    }
+}
