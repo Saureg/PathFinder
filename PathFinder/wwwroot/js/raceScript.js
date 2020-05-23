e7b27384cba2ac06b893d32668b9f5442ef4c645
Egor Uvarov <e.uvarov@pravo.ru>
2020-05-23 13:00:38 +0400

    Race script

---
+++ PathFinder/wwwroot/js/raceScript.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(
+    function() {
+    $( "#AnyTrait" ).change(disableTraits);
+})
+
+$(document).ready(
+    function () {
+        $('[class~="trait"]').change()
+    }
+)
+
+$(document).ready(disableTraits)
+$(document).ready(disableAnyTrait)
+
+function disableTraits() {
+    let anyTraitValue = $("#AnyTrait").val();
+    if (anyTraitValue === "0"){
+        $('[class~="trait"]').prop("disabled", false)
+    }
+    else {
+        $('[class~="trait"]').prop("disabled", true)
+    }
+}
+
+function disableAnyTrait() {
+    let traits = $('[class~="trait"]');
+    let isEmptyVal = true;
+
+    traits.each(function () {
+        if ($(this).val() !== "0"){
+            isEmptyVal = false;
+            return false;
+        }
+    })
+
+    if (isEmptyVal === true){
+        $( "#AnyTrait" ).prop("disabled", false)
+    }
+    else {
+        $( "#AnyTrait" ).prop("disabled", true)
+    }
+}
